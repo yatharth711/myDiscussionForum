c64fc0b815eb75e26617dd95360397a6aa536421
Tithi Soni <tithi2k3@gmail.com>
2024-03-25 23:44:11 -0700

    db bug fix

--- myDiscussionForum-main/connectionDB.php
+++ myDiscussionForum-main/connectionDB.php
@@ -12,7 +12,7 @@ $password = '80772049';
 $dbname = 'db_80772049'; //db name here
 */
 //Create connection
-    $conn = new mysqli_connect($hostname, $username, $password, $database);
+    $conn = mysqli_connect($hostname, $username, $password, $database);
 
     if(mysqli_connect_error()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
