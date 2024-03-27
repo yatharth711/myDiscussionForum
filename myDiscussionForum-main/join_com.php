932c44fbad642c79cb37c1e0ad4e7ab1d4717d21
boluya <119131124+boluya@users.noreply.github.com>
2024-03-26 19:29:13 -0700

    Update join_com.php

--- myDiscussionForum-main/join_com.php
+++ myDiscussionForum-main/join_com.php
@@ -1,34 +1,27 @@
-<?php
-session_start();
-if(empty($_SESSION["uid"])){
-    header("Location: login.php");
-}
-
-if(isset($_SESSION["uid"])){
-
-include_once 'connectionDB.php';
-
-// Get the user ID from the current session
-$uid = $_SESSION['uid'];
-
-// Get the community ID from the submitted form data
-$com_id = $_POST['com_id'];
-
-$query = "SELECT * FROM user_communities WHERE uid = '$uid' AND com_id = '$com_id'";
-$result = mysqli_query($conn,$query);
-if(mysqli_num_rows($result) == 0){
-    // Insert a new row into the user_communities table
-
-    $query = "INSERT INTO user_communities (uid, com_id) VALUES (?, ?)";
-    $stmt = mysqli_prepare($conn, $query);
-    mysqli_stmt_bind_param($stmt, "ii", $uid, $com_id);
-    $result = mysqli_stmt_execute($stmt);
-    
-}
-
-// Close the database connection
-mysqli_close($conn);
-
-header("Location: home.php");
-}
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Join a Community</title>
+</head>
+<body>
+    <h1>Join a Community</h1>
+    <form action="process_join.php" method="post">
+        <label for="community">Select a community:</label>
+        <select id="community" name="community">
+            <?php
+            include 'connectionDB.php';
+            
+            $query = "SELECT * FROM communities";
+            $result = mysqli_query($conn, $query);
+            
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<option value='" . $row['com_id'] . "'>" . $row['name'] . "</option>";
+            }
+            
+            mysqli_close($conn);
+            ?>
+        </select>
+        <input type="submit" value="Join Community">
+    </form>
+</body>
+</html>
